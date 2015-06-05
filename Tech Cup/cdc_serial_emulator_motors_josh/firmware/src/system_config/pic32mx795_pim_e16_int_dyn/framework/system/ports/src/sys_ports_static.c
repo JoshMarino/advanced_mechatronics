3eea2f914c81779ca3e8e111c1faca8c8fd38f66
Josh Marino <joshuamarino2015@u.northwestern.edu>
2015-06-05 14:35:06 -0500

    trying a new folder

---
+++ Tech Cup/cdc_serial_emulator_motors_josh/firmware/src/system_config/pic32mx795_pim_e16_int_dyn/framework/system/ports/src/sys_ports_static.c
@@ -0,0 +1,18 @@
+#include "system_config.h"
+#include "peripheral/ports/plib_ports.h"
+
+
+void SYS_PORTS_Initialize(void)
+{
+//    /* PORT B Initialization */
+//    //USER Button B13
+//    ANSELBbits.ANSB13 = 0;      // 0 for digital, 1 for analog
+//    PLIB_PORTS_DirectionOutputSet( PORTS_ID_0, PORT_CHANNEL_B,  0b10000000000000);
+//
+//    //Yellow LED3 B7
+//    PLIB_PORTS_DirectionOutputSet( PORTS_ID_0, PORT_CHANNEL_B,  0b10000000);
+//
+//    //Green LED2 B15
+//    ANSELBbits.ANSB15 = 0;      // 0 for digital, 1 for analog
+//    PLIB_PORTS_DirectionOutputSet( PORTS_ID_0, PORT_CHANNEL_B,  0b1000000000000000);
+}
